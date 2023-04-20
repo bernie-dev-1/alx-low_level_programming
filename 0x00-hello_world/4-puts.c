055d9ffea955137fd3e13d9d09bffcf94babcd91
bernie-dev-1 <bernyx.owusu@gmail.com>
2023-04-20 16:09:21 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that outpus info with puts method
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
