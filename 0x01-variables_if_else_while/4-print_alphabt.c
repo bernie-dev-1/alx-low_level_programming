2199db5acd50441d0568754a39a6436a7f0bae4a
bernie-dev-1 <bernyx.owusu@gmail.com>
2023-04-21 19:29:08 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (n == 101 || n == 113)
+		{
+			continue;
+		}
+
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
